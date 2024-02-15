7a0a29099fd3e83e5c78196381e7004c3900dbc3
Melroy Pinto <melroychethanpinto@gmail.com>
2024-02-15 21:40:32 +0530

    Added Jenkins Image into the site

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def index():
     # List of image URLs
     image_urls = [
+        'https://www.jenkins.io/images/logo-title-opengraph.png',
         'https://bunnyacademy.b-cdn.net/what-is-docker.png',
         'https://miro.medium.com/v2/resize:fit:600/1*Pbb5rmrwh-eAFWXd8ws79A.png',
         # Add more image URLs as needed
